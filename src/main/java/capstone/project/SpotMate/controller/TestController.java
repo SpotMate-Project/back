d0b1bce19228e24ca3ed0e31f0cccf3e1fb7969a
박현수 <pm6567@naver.com>
2024-04-12 00:29:50 +0900

    feat : TestController 추가 서버 load확인

---
+++ src/main/java/capstone/project/SpotMate/controller/TestController.java
@@ -0,0 +1,14 @@
+package capstone.project.SpotMate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "test는 성공적";
+    }
+}
