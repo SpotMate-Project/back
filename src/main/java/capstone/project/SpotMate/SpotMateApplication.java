3dd74d53740d03ca70b77b3c87daf5918856f618
박현수 <pm6567@naver.com>
2024-04-11 23:59:46 +0900

    feat : ouath2 기본 로그인 화면 off

--- src/main/java/capstone/project/SpotMate/SpotMateApplication.java
+++ src/main/java/capstone/project/SpotMate/SpotMateApplication.java
@@ -2,8 +2,9 @@ package capstone.project.SpotMate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpotMateApplication {
 
 	public static void main(String[] args) {
