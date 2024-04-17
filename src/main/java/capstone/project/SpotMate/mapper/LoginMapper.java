158c75d806838bc3366e94f78f525a95987a2f08
박현수 <pm6567@naver.com>
2024-04-18 02:07:42 +0900

    feat : LoginMapper -> findUserbyEmail 작성

---
+++ src/main/java/capstone/project/SpotMate/mapper/LoginMapper.java
@@ -0,0 +1,12 @@
+package capstone.project.SpotMate.mapper;
+
+import capstone.project.SpotMate.dto.LoginDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface LoginMapper {
+
+    LoginDTO findUserByEmail(String email);
+}
