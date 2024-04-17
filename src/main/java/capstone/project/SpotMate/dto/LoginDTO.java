cd7822882542db201e2d0b0b3eefaca15b73a351
박현수 <pm6567@naver.com>
2024-04-18 02:06:33 +0900

    feat : LoginDTO 등록

---
+++ src/main/java/capstone/project/SpotMate/dto/LoginDTO.java
@@ -0,0 +1,17 @@
+package capstone.project.SpotMate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+
+    public String id= null;
+
+    public String email =null;
+
+    public String password =null;
+}
