b992bb861c34259204d16617421b076c2c9c8860
박현수 <pm6567@naver.com>
2024-05-10 00:10:07 +0900

    feat : lang3 의존성 추가

---
+++ src/main/java/capstone/project/SpotMate/configure/utils/Datetime.java
@@ -0,0 +1,12 @@
+package blog.blovi.app.utils;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class Datetime {
+    private static final ZoneId KST_ZONE_ID = ZoneId.of("Asia/Seoul");
+
+    public static LocalDateTime nowInKst() {
+        return LocalDateTime.now(KST_ZONE_ID);
+    }
+}
