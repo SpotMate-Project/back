f76735ce20e5666c4d4dbaef01f25c219d2bcd3b
박현수 <pm6567@naver.com>
2024-05-29 22:13:10 +0900

    feat: id,email,nickname가져오도록 수정

---
+++ src/main/java/capstone/project/SpotMate/dto/UserInfoDTO.java
@@ -0,0 +1,19 @@
+package capstone.project.SpotMate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@Setter
+@AllArgsConstructor
+@Data
+public class UserInfoDTO {
+    private Integer id;
+    private String email;
+
+    private String nickname;
+}
